21ed14f919da032dcf356f77d0e7c42aa3294fd3
rokib97 <hasanrokib1997@gmail.com>
2023-07-09 16:37:30 +0600

    HTTP Request and response

--- src/server.js
+++ src/server.js
@@ -2,6 +2,12 @@ const express = require("express");
 const app = express();
 const port = 3001;
 
+app.get("/test", (req, res) => {
+  res.status(200).send({
+    message: "API is working fine!",
+  });
+});
+
 app.listen(port, () => {
   console.log(`E commerce mern server is running on port ${port}`);
 });
